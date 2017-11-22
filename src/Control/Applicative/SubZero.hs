3d762b93bc02e4e2ef8d9b0a663d9e9e6d00d9ca
Tristan Wibberley <tristan.wibberley@gmail.com>
2017-11-22 19:45:37 +0000

    Add some docs, a test fizzbuzz program, and reimplemented SubZero in terms of Compose

--- src/Control/Applicative/SubZero.hs
+++ src/Control/Applicative/SubZero.hs
@@ -16,11 +16,27 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 -}
 
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
+{- |
+    Module      : Control.Applicative.SubZero
+    Description : SubZero type and related functions
+    Copyright   : (c) Tristan Wibberley, 2017
+    License     : GPL-2
+    Maintainer  : tristan.wibberley@gmail.com
+    Stability   : experimental
+
+    Here is a longer description of this module, containing some
+    commentary with @some markup@.
+-}
 module Control.Applicative.SubZero
     ( SubZero
+      -- * Constructors and Translators
+      -- 
+      -- $constructors
+    , points
     , foldlA1
     , letStand
-    , points
     , fromSubZero
     , collapse
     ) where
@@ -29,30 +45,32 @@ import Control.Applicative
 import Data.Monoid
 import Data.Maybe
 import Data.Function
+import Data.Functor.Compose
 
--- SubZero
--- f = major functor
--- g = superposition policy - how do they collapse?
--- a = transformed value type
-newtype SubZero f g a = SubZero { getSubZero :: f (g a) }
-instance (Functor f, Functor g) => Functor (SubZero f g) where
-  fmap f s = SubZero $ f' <$> a
-    where a = getSubZero s
-          f' = fmap f
+{- |
+   Converts a functor so that each point at the source has alternatives.
 
-instance (Applicative f, Applicative g) => Applicative (SubZero f g) where
-  pure v = (SubZero . pure . pure) v
-  r <*> s = SubZero $ f' <*> b
-    where a = getSubZero r
-          b = getSubZero s
-          f' = fmap (<*>) a
+   It's just like Compose but the applicative instance appends new
+   alternative values in the rightmost (inner/minor) functor instead of
+   in the leftmost (outer/major) functor.
+
+   The result is that two ZipLists of alternatives zip together,
+   providing alternatives to each point and two
+
+   Given the immediate utility of this, I do wonder if the Alternative
+   instance of Compose is the wrong one.
+
+  f = major functor
+  g = superposition policy - how do they collapse?
+  a = transformed value type
+-}
+newtype SubZero f g a =
+  SubZero { getSubZero :: Compose f g a }
+  deriving (Show, Read, Eq, Ord, Functor, Applicative)
 
 instance (Applicative f, Alternative g) => Alternative (SubZero f g) where
-  empty   = SubZero $ pure empty
-  r <|> s = SubZero $ f' <*> b
-    where a = getSubZero r
-          b = getSubZero s
-          f' = fmap (<|>) a
+  empty   = SubZero $ Compose $ pure empty
+  (SubZero (Compose a)) <|> (SubZero (Compose b)) = SubZero $ Compose $ (<|>) <$> a <*> b
 
 -- A foldl for foldable alternatives, empty stays empty, nonempty becomes one alternative
 -- The applicative you get back can be a different type than the input
@@ -66,17 +84,41 @@ letStand :: (Alternative f) => (a -> Bool) -> a -> f a
 letStand f x | f x       = pure x
              | otherwise = empty
 
---    Turns a container of values to a container of either
--- still-standing or destroyed values based on a predicate
---    The type constraint allows us to model possible outcomes
--- where destroyed values are just "no possible outcomes"
-points :: (Functor f, Alternative g) => (a -> Bool) -> f a -> SubZero f g a
-points f = SubZero . ((letStand f) <$>)
+{- $constructors
+
+   Constructors are good
+-}
+
+{- | Turns a container of values to a container of either
+   retained or destroyed values based on a predicate
+
+     The type constraint allows us to model possible outcomes
+   so destroyed values are just "no possible outcomes" while
+   retained values represent "the only possible outcome".
+
+     To represent that "no value" is a possible outcome, @a@
+   should be some type like ('Maybe' a) or ('Either' 'String' a).
+
+   [@f@]: This functor defines the broad scale
+          behaviours but its Alternative instance is overridden.
+          This in particular might change during upcoming design
+          validation.
+   [@g@]: This functor supplies the supercedent Alternative
+          instance and thus the finer behaviours.
+-}
+points :: 
+  (Functor f, Alternative g)
+    => (a -> Bool)   -- ^ A predicate that indicates whether a point
+                     -- is occupied by its original value or vacant.
+    -> f a           -- ^ The seed points with their values.
+    -> SubZero f g a -- ^ The constructed @SubZero@ value.
+points f = SubZero . Compose . ((letStand f) <$>)
+
 
 --    If the type of the possibilities concept is Maybe then you can
 -- use fromSubZero with a default for impossible points
 fromSubZero :: (Applicative f) => f a -> SubZero f Maybe a -> f a
-fromSubZero a s = fromMaybe <$> a <*> (getSubZero s)
+fromSubZero a (SubZero (Compose b)) = fromMaybe <$> a <*> b
 
 --     Take the alternatives embedded in the SubZero and collapse them
 -- with a collapse function to a single alternative or empty (no possible outcomes)
@@ -85,7 +127,5 @@ fromSubZero a s = fromMaybe <$> a <*> (getSubZero s)
 -- retain behaviours of more sophisticated types. A consequence of this
 -- is that you will probably require to state a type
 collapse :: (Functor f, Foldable g, Alternative h) => (a -> a -> a) -> SubZero f g a -> SubZero f h a
-collapse f s = SubZero $ f' <$> a
-  where a = getSubZero s
-        f' = foldlA1 f
+collapse f (SubZero (Compose a)) = SubZero $ Compose $ (foldlA1 f) <$> a
 
--- test/Spec.hs
+++ test/Spec.hs
@@ -16,5 +16,33 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 -}
 
+{-# LANGUAGE PartialTypeSignatures #-}
+
+import Control.Applicative
+import Control.Applicative.SubZero
+import System.Exit
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+
+fizzbuzz indexes = let isMultiple n x = x `mod` n == 0 in
+
+                   let numbers = show <$> indexes in
+                   let fizzpoints = points (isMultiple 3) indexes in
+                   let fizzes = "Fizz" <$ fizzpoints in
+                   let buzzpoints = points (isMultiple 5) indexes in
+                   let buzzes = "Buzz" <$ buzzpoints in
+
+
+                   let substitutions :: SubZero _ [] _
+                       substitutions = fizzes <|> buzzes in
+
+                   let words = collapse (++) substitutions in
+                   fromSubZero numbers words
+
+main = let indexes = ZipList [1..] in -- mostly lattice-like uses, not nondeterminism
+       let result = fizzbuzz indexes in
+       let check_answers = take 15 $ getZipList $ result in
+       do putStrLn $ show check_answers
+          if check_answers == ["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]
+           then exitSuccess
+           else exitFailure
