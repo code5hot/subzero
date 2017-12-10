a7df72acc90f74ea5a02577b587577ca5b6053b7
Tristan Wibberley <tristan.wibberley@gmail.com>
2017-12-10 21:10:53 +0000

    uprev. Added an intersectWith implementation for IMap. Refactor

--- src/Data/Map/IMap.hs
+++ src/Data/Map/IMap.hs
@@ -35,39 +35,50 @@ module Data.Map.IMap
 import qualified Data.Map as M
 import qualified Data.IntMap as IM
 
-{- | An applicative map that discards mismatched keys (ie, uses intersection)
- - -}
+{- | An applicative map that discards mismatched keys
+     ie, it uses intersection for it's applicative instance
+
+     It has a case in which every key is occupied and equal
+-}
 newtype IMap m v = IMap (Either v (m v))
   deriving (Show, Eq)
 
-class Functor m => IsMap m where
+{- | A class for maps so IMap can be generic
+-}
+class IsMap m where
   type Key m
   fromList         :: [(Key m, v)] -> m v
   intersectionWith :: (a -> b -> c) -> m a -> m b -> m c
-
+  mapmap           :: (a -> b) -> m a -> m b
 
 instance Ord k => IsMap (M.Map k) where
   type Key (M.Map k) = k
   fromList           = M.fromList
   intersectionWith   = M.intersectionWith
+  mapmap             = M.map
 
 instance IsMap IM.IntMap where
   type Key IM.IntMap = IM.Key
   fromList           = IM.fromList
   intersectionWith   = IM.intersectionWith
-
-instance Functor m => Functor (IMap m) where
-  fmap f (IMap (Left  v)) = IMap (Left $ f v)
-  fmap f (IMap (Right m)) = IMap (Right $ fmap f m)
-
-instance IsMap m => Applicative (IMap m) where
-  pure v                            = IMap (Left v)
-  IMap (Left  f) <*> b              = fmap f b
-  a              <*> IMap (Left  v) = fmap ($v) a
-  IMap (Right m) <*> IMap (Right v) = IMap (Right $ intersectionWith ($) m v)
+  mapmap             = IM.map
 
 instance IsMap m => IsMap (IMap m) where
   type Key (IMap m) = Key m
   fromList l        = IMap (Right $ fromList l)
-  intersectionWith  = undefined
+
+  intersectionWith f (IMap (Left  u)) (IMap (Left  v)) = IMap (Left $ f u v)
+  intersectionWith f (IMap (Left  u)) (IMap (Right b)) = IMap (Right $ mapmap (     f u) b)
+  intersectionWith f (IMap (Right a)) (IMap (Left  v)) = IMap (Right $ mapmap (flip f v) a)
+  intersectionWith f (IMap (Right a)) (IMap (Right b)) = IMap (Right $ intersectionWith f a b)
+
+  mapmap f (IMap (Left  v)) = IMap (Left $ f v)
+  mapmap f (IMap (Right a)) = IMap (Right $ mapmap f a)
+ 
+instance IsMap m => Functor (IMap m) where
+  fmap = mapmap
+
+instance IsMap m => Applicative (IMap m) where
+  pure v = IMap (Left v)
+  (<*>)  = intersectionWith ($)
 
